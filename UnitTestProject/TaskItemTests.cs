e6d1a69acca67d3d9f58f02d12f352ae2e3e57d9
Mondongo-cl <ivan.rosales.rieloff@gmail.com>
2020-06-30 20:42:23 -0400

    Simple statefull service

---
+++ UnitTestProject/TaskItemTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoList.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList.Domain.Tests
+{
+    [TestClass()]
+    public class TaskItemTests
+    {
+        [TestMethod()]
+        public void TaskItemTest()
+        {
+            TaskItem t = new TaskItem {
+                Description=null,
+                Id=Guid.Empty,
+                Name=null,
+                Status=0
+            };
+
+            Assert.IsNotNull(t);
+        }
+    }
+}
