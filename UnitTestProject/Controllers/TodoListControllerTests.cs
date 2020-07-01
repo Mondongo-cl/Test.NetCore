e86c3ca4f07ea024899ed2556ac9fea051e3a5a8
Mondongo-cl <ivan.rosales.rieloff@gmail.com>
2020-07-01 14:41:52 -0400

    test

--- UnitTestProject/Controllers/TodoListControllerTests.cs
+++ UnitTestProject/Controllers/TodoListControllerTests.cs
@@ -4,34 +4,34 @@ using System.Linq;
 
 namespace TodoList.WebApi.Controllers.Tests
 {
-    [TestClass()]
-    public class TodoListControllerTests
-    {
-        [TestMethod()]
-        public void GetAllTest()
-        {
-            TodoListController c = new TodoListController();
-            var data = c.Get();
-            Assert.IsNotNull(data.ToArray());
-            Assert.AreNotSame(0, data.Count());
-        }
+    //[TestClass()]
+    //public class TodoListControllerTests
+    //{
+    //    [TestMethod()]
+    //    public void GetAllTest()
+    //    {
+    //        TodoListController c = new TodoListController();
+    //        var data = c.Get();
+    //        Assert.IsNotNull(data.ToArray());
+    //        Assert.AreNotSame(0, data.Count());
+    //    }
 
-        [TestMethod]
-        public void AddNewItem()
-        {
-            TodoListController c = new TodoListController();
-            c.Post(new TaskItem {
-                Description="Unit Test",
-                Id = Guid.NewGuid(),
-                Name="My Task Name",
-                Status= Domain.TaskItemStatus.Caceled
-            }).Wait();
+    //    [TestMethod]
+    //    public void AddNewItem()
+    //    {
+    //        TodoListController c = new TodoListController();
+    //        c.Post(new TaskItem {
+    //            Description="Unit Test",
+    //            Id = Guid.NewGuid(),
+    //            Name="My Task Name",
+    //            Status= Domain.TaskItemStatus.Caceled
+    //        }).Wait();
 
-            var data = c.Get();
+    //        var data = c.Get();
 
-            Assert.IsNotNull(data.ToArray());
-            Assert.IsTrue(0 < data.Count());
+    //        Assert.IsNotNull(data.ToArray());
+    //        Assert.IsTrue(0 < data.Count());
 
-        }
-    }
+    //    }
+    //}
 }
--- UnitTestProject/TodoItemRespositoryTests.cs
+++ UnitTestProject/TodoItemRespositoryTests.cs
@@ -1,136 +1,66 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TodoListService;
 using System;
+using System.Collections.Generic;
+using System.Text;
 using ServiceFabric.Mocks;
-using ServiceFabric.Mocks.ReplicaSet;
-
 using System.Linq;
-using System.Fabric;
-using Microsoft.ServiceFabric.Data;
-using Microsoft.ServiceFabric.Data.Collections;
-using TodoList.Domain;
-// using Microsoft.ServiceFabric.Data;
-// using System.Fabric;
-
-
 
 namespace TodoListService.Tests
 {
-
-
-    //[TestClass()]
-    //public class TodoItemRespositoryTests
-    //{
-
-
-    //    [TestMethod()]
-    //    public async System.Threading.Tasks.Task AddTaskItemTest()
-    //    {
-
-    //        var context = MockStatefulServiceContextFactory.Default;
-    //        var stateManager = new MockReliableStateManager();
-    //        var service = new TodoListService(context, stateManager);
-
-    //        //get state
-    //        var payloads = await stateManager.TryGetAsync<IReliableDictionary<string, TaskItem>>("todo-list");
-
-
-
-
-
-
-
-    //        //Func<StatefulServiceContext, IReliableStateManagerReplica2, TodoListService> 
-    //        ////var 
-    //        //    CreateStatefulService = 
-    //        //    (context,stateManager) => new TodoListService(context);
-
-    //        //Func<StatefulServiceContext, ServiceFabric.Mocks.ReliableCollections.TransactedConcurrentDictionary<Uri, IReliableState>, IReliableStateManagerReplica2> 
-    //        ////var
-    //        //    stateManager 
-    //        //    = (context,serviceURL) => { 
-    //        //        return new MockReliableStateManager(serviceURL); 
-    //        //    };
-
-    //        // var replicaSet = new MockStatefulServiceReplicaSet<TodoListService>(CreateStatefulService, stateManager);
-
-
+    [TestClass()]
+    public class TodoItemRespositoryTests
+    {
+        [TestMethod()]
+        public void AddTaskItemTest()
+        {
+            MockReliableStateManager manager = new MockReliableStateManager();
             
-    //        //await replicaSet.AddReplicaAsync(ReplicaRole.Primary, 1);
-    //        //await replicaSet.AddReplicaAsync(ReplicaRole.ActiveSecondary, 2);
-    //        //await replicaSet.AddReplicaAsync(ReplicaRole.ActiveSecondary, 3);
+            TodoItemRespository repo = new TodoItemRespository(manager);
 
-    //        TodoList.Domain.TaskItem myItem = new TodoList.Domain.TaskItem
-    //        {
-    //            Id = Guid.NewGuid(),
-    //            Description = "",
-    //            Name = "",
-    //            Status = TodoList.Domain.TaskItemStatus.InProgress
-    //        };
+            TodoList.Domain.TaskItem myItem = new TodoList.Domain.TaskItem
+            {
+                Id = Guid.NewGuid(),
+                Description = "",
+                Name = "",
+                Status = TodoList.Domain.TaskItemStatus.InProgress
+            };
+            var t = repo.AddTaskItem(myItem).GetAwaiter();
 
+            t.GetResult();
 
-            
-    //        await service.AddTaskItemAsync(myItem);
+            Assert.IsNotNull(repo.GetAllTaskItems().Result);
+            var otherItem = repo.GetAllTaskItems().Result.FirstOrDefault();
+            Assert.AreSame(otherItem, myItem);
 
-    //        //Assert.IsTrue(payloads == 1);
-    //        // Assert.IsTrue(payloads[0].Id != Guid.Empty);
+        }
 
-    //        //verify the data was saved against the reliable dictionary
-    //        // var dictionary = await stateManager.GetOrAddAsync<IReliableDictionary<string, Payload>>(MyStatefulService.StateManagerDictionaryKey);
-    //        //using (var tx = ((IReliableStateManager)stateManager).CreateTransaction())
-    //        //{
-    //        //    var payload = await dictionary.TryGetValue(stateName);
-    //        //    Assert.IsTrue(payload.HasValue);
-    //        //    Assert.IsTrue(payload.Value.Content == payload.Content);
-    //        //}
+        [TestMethod()]
+        public void GetAllTaskItemsTest()
+        {
+            MockReliableStateManager manager = new MockReliableStateManager();
+            TodoItemRespository repo = new TodoItemRespository(manager);
 
+            TodoList.Domain.TaskItem myItem = new TodoList.Domain.TaskItem
+            {
+                Id = Guid.NewGuid(),
+                Description = "",
+                Name = "",
+                Status = TodoList.Domain.TaskItemStatus.InProgress
+            };
 
-    //        //MockReliableStateManager manager = new MockReliableStateManager();
-    //        //TodoItemRespository repo = new TodoItemRespository(manager);
+            var t = repo
+                .AddTaskItem(myItem)
+                .ContinueWith(
+                (ct) => {
+                    myItem.Id = Guid.NewGuid();
+                    repo.AddTaskItem(myItem).GetAwaiter().GetResult();
+                });
 
-    //        //TodoList.Domain.TaskItem myItem = new TodoList.Domain.TaskItem
-    //        //{
-    //        //    Id = Guid.NewGuid(),
-    //        //    Description = "",
-    //        //    Name = "",
-    //        //    Status = TodoList.Domain.TaskItemStatus.InProgress
-    //        //};
-    //        //var t = repo.AddTaskItem(myItem).GetAwaiter();
-
-    //        //t.GetResult();
-
-    //        //Assert.IsNotNull(repo.GetAllTaskItems().Result);
-    //        //var otherItem = repo.GetAllTaskItems().Result.FirstOrDefault();
-    //        //Assert.AreSame(otherItem, myItem);
-
-    //    }
-
-    //    [TestMethod()]
-    //    public void GetAllTaskItemsTest()
-    //    {
-    //        MockReliableStateManager manager = new MockReliableStateManager();
-    //        TodoItemRespository repo = new TodoItemRespository(manager);
-
-    //        TodoList.Domain.TaskItem myItem = new TodoList.Domain.TaskItem
-    //        {
-    //            Id = Guid.NewGuid(),
-    //            Description = "",
-    //            Name = "",
-    //            Status = TodoList.Domain.TaskItemStatus.InProgress
-    //        };
-
-    //        var t = repo
-    //            .AddTaskItem(myItem)
-    //            .ContinueWith(
-    //            (ct) => {
-    //                myItem.Id = Guid.NewGuid();
-    //                repo.AddTaskItem(myItem).GetAwaiter().GetResult();
-    //            });
-
-    //        Assert.IsNotNull(repo.GetAllTaskItems().Result);
-    //        var otherItem = repo.GetAllTaskItems().Result;
-    //        Assert.IsTrue(otherItem.Count() > 0);
-    //        Assert.IsTrue(otherItem.Contains(myItem));
-    //    }
-    //}
+            Assert.IsNotNull(repo.GetAllTaskItems().Result);
+            var otherItem = repo.GetAllTaskItems().Result;
+            Assert.IsTrue(otherItem.Count() > 0);
+            Assert.IsTrue(otherItem.Contains(myItem));
+        }
+    }
 }
