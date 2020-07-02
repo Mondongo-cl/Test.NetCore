30b42268e8d1cb1f6e9759fdd30b871acf195426
Mondongo-cl <ivan.rosales.rieloff@gmail.com>
2020-07-02 13:14:45 -0400

    ServiceEventSource basics Tests

---
+++ UnitTestProject/EventSourceTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoListService;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class EventSourceTests
+    {
+        [TestMethod]
+        public void TestMessage()
+        {
+
+            var c =
+                ServiceEventSource.Current;
+            Assert.IsNotNull(c);
+            c.Message("Send Message");
+        }
+
+        [TestMethod]
+        public void TestWrite()
+        {
+
+            var c =
+                ServiceEventSource.Current;
+            Assert.IsNotNull(c);
+            c.Write("Dummy Event");
+        }
+    }
+}
